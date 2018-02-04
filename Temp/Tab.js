fc914159dbc99108cf30c3ac3ea7b9331fbc91c1
quangminhtrancal <leap.ahead.tran@gmail.com>
2018-02-04 11:06:59 -0700

    Add files via upload

---
+++ Temp/Tab.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { AppRegistry, asset, Pano, Text, View, StyleSheet, ListView } from 'react-native';
+import { Tabbar } from './Router';
+
+export default class App extends Component {
+
+
+ 
+  render() {
+    return (
+        <Tabbar />
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
+    
+  },
+
+});
